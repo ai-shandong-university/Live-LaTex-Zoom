b3d08ab53942ead5fe906af69eb6af843954e3e9
Inno Jia <jdgets111@gmail.com>
2019-03-28 15:37:33 +0800

    Add vue-cil usage Thu Mar 28 15:37:33 CST 2019

---
+++ usage-demo/vue-test/my-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <BasicUsage msg="可以开始了吗..."></BasicUsage>
+  </div>
+</template>
+
+<script>
+import BasicUsage from './components/BasicUsage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BasicUsage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
